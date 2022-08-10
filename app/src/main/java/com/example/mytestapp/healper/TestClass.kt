4276f51d4b44abc4e58848413573c779328f89fd
RahulSharmaTelus <rahul.sharma13@telusinternational.com>
2022-08-10 12:51:41 +0530

    Checking multiple artifact upload

---
+++ app/src/main/java/com/example/mytestapp/healper/TestClass.kt
@@ -0,0 +1,10 @@
+package com.example.mytestapp.healper
+
+/**
+ * This is test class taking a string argument country.
+ */
+class TestClass constructor(country: String) {
+
+    var abc = country
+
+}
