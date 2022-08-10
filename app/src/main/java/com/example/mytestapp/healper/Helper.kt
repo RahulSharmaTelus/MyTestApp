ea343beccc16b8aca2e9ca152b570dc90455ec08
RahulSharmaTelus <rahul.sharma13@telusinternational.com>
2022-08-10 13:54:41 +0530

    Checking multiple artifact upload 2

--- app/src/main/java/com/example/mytestapp/healper/Helper.kt
+++ app/src/main/java/com/example/mytestapp/healper/Helper.kt
@@ -1,14 +1,12 @@
 package com.example.mytestapp.healper
 
 object Helper {
-    var value: Int = 10
 
     /**
      * Check if the given number is high
      * or low.
      */
     fun getText(num: Int): String {
-        value = num
         if (num >= 100)
             return "High"
         else
